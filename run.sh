a519142a63a17de71d9a3fa968eb4e7e60f9e759
Ruchika Sah <ruchika817@users.noreply.github.com>
2021-06-23 10:53:47 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,45 @@
+REARC_DATA_PLATFORM_ROLE_ARN='arn:aws:iam::412981388937:role/CrossAccountRole-796406704065-796406704065'
+REARC_DATA_PLATFORM_EXTERNAL_ID='Rearc-Data-Platform-796406704065'
+ASSET_BUCKET='rearc-data-provider'
+MANIFEST_BUCKET='rearc-control-plane-manifest'
+CUSTOMER_ID='796406704065'
+DATASET_NAME='test-dataset'
+DATASET_ARN='arn:aws:dataexchange:us-east-1:796406704065:data-sets/6afda64ca351e462006618317d6c2f7b'
+PRODUCT_NAME='Test'
+PRODUCT_ID='prod-ug6qz34kqdxco'
+SCHEDULE_CRON="cron(30 5 ? * 6 *)"
+REGION='us-east-1'
+PROFILE='default'
+
+echo "------------------------------------------------------------------------------"
+echo "RearcDataPlatformRoleArn: $REARC_DATA_PLATFORM_ROLE_ARN"
+echo "RearcDataPlatformExternalId: $REARC_DATA_PLATFORM_EXTERNAL_ID"
+echo "CustomerId: $CUSTOMER_ID"
+echo "AssetBucket: $ASSET_BUCKET"
+echo "ManifestBucket: $MANIFEST_BUCKET"
+echo "DataSetName: $DATASET_NAME"
+echo "DataSetArn: $DATASET_ARN"
+echo "ProductName: $PRODUCT_NAME"
+echo "ProductID: $PRODUCT_ID"
+echo "ScheduleCron: $SCHEDULE_CRON"
+echo "Region: $REGION"
+echo "PROFILE: $PROFILE"
+echo "------------------------------------------------------------------------------"
+
+
+# python pre-processing/pre-processing-code/source_data.py
+
+./init.sh \
+    --rdp-role-arn "${REARC_DATA_PLATFORM_ROLE_ARN}" \
+    --rdp-external-id "${REARC_DATA_PLATFORM_EXTERNAL_ID}" \
+    --customer-id "${CUSTOMER_ID}" \
+    --schedule-cron "${SCHEDULE_CRON}" \
+    --asset-bucket "${ASSET_BUCKET}" \
+    --manifest-bucket "${MANIFEST_BUCKET}" \
+    --dataset-name "${DATASET_NAME}" \
+    --product-name "${PRODUCT_NAME}" \
+    --product-id "${PRODUCT_ID}" \
+    --dataset-arn "${DATASET_ARN}" \
+    --region "${REGION}" \
+    --first-revision "false" \
+    --profile "${PROFILE}"
